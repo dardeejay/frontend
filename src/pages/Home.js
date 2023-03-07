b6a41f65448f57a2e64e59a44fa6fbce65469af8
Dardee Jay C. Calura <68296049+dardeejay@users.noreply.github.com>
2023-03-07 12:18:23 +0800

    fix env

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,20 @@
+
+
 import React from 'react'
 import { useEffect } from 'react'
+
 import { useWorkoutContext } from '../hooks/useWorkoutContext'
 // components
 import WorkoutForm from '../components/WorkoutForm'
 import WorkoutDetails from '../components/WorkoutDetails'
+
+const API_URL = process.env.REACT_APP_API_URL
 export default function Home() {
   const {workouts, dispatch} = useWorkoutContext()
 
   useEffect(()=>{
     const fetchWorkouts = async () => {
-      const res = await fetch('https://workouts-api-dj.onrender.com//api/workouts')
+      const res = await fetch(`${API_URL}/api/workouts`)
       const data = await res.json()
       if(res.ok){
         dispatch({type: 'SET_WORKOUTS', payload: data})
